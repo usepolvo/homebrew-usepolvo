ae6131aed141dd5c91e4ed2de9902a80fdc641e5
Caio Pizzol <caio@harbourshare.com>
2024-08-01 17:34:55 -0300

    chore: update usepolvo-cli

--- Formula/usepolvo.rb
+++ Formula/usepolvo.rb
@@ -3,8 +3,8 @@ class Usepolvo < Formula
 
   desc "An all-in-one integration CLI package."
   homepage "https://github.com/usepolvo/usepolvo-cli"
-  url "https://github.com/usepolvo/usepolvo-cli/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "2b2e763c076ffaf28b5def40d2686f52968e15f4bbcdc297a74d40343fb63c17"
+  url "https://github.com/usepolvo/usepolvo-cli/archive/refs/tags/v0.1.7.tar.gz"
+  sha256 "25179a79588662e6e81104bb8a36a11b30a83f57364f3af9e355986eb9b47baf"
   license "MIT"
 
   depends_on "python@3.9"
@@ -19,15 +19,21 @@ class Usepolvo < Formula
     sha256 "b3064a35807dcf6738aecc3c60856ba8013d1735fe94efb19fd9f40704121722"
   end
 
+  resource "python-dotenv" do
+    url "https://files.pythonhosted.org/packages/bc/57/e84d88dfe0aec03b7a2d4327012c1627ab5f03652216c63d49846d7a6c58/python-dotenv-1.0.1.tar.gz"
+    sha256 "e324ee90a023d808f1959c46bcbc04446a10ced277783dc6ee09987c37ec10ca"
+  end
+
   resource "stripe" do
     url "https://files.pythonhosted.org/packages/bb/da/bd8c4be9099fea125fb44fe88a17b4dddd9e20506c3ee46fd47090a1ffef/stripe-10.5.0.tar.gz"
     sha256 "4e7966a2bba0095354ae00d6dc352c2b58646f1ac258a9c28bf56973ae102bb1"
   end
 
-  resource "python-dotenv" do
-    url "https://files.pythonhosted.org/packages/bc/57/e84d88dfe0aec03b7a2d4327012c1627ab5f03652216c63d49846d7a6c58/python-dotenv-1.0.1.tar.gz"
-    sha256 "e324ee90a023d808f1959c46bcbc04446a10ced277783dc6ee09987c37ec10ca"
+  resource "typing_extensions" do
+    url "https://files.pythonhosted.org/packages/df/db/f35a00659bc03fec321ba8bce9420de607a1d37f8342eee1863174c69557/typing_extensions-4.12.2.tar.gz"
+    sha256 "1a7ead55c7e559dd4dee8856e3a88b41225abfe1ce8df57b7c13915fe121ffb8"
   end
+
 
   def install
     # Move contents of src to the root
